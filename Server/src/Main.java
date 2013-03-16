9b7e190c7016dce3447b106288dd085bedbf7f81
Taneong <tigrankryukov@gmail.com>
2013-03-17 02:31:52 +0400

    Added Main & Settings classes

---
+++ Server/src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Основной класс сервера EasyShot.
+ * TODO:
+ * - Прослушка порта
+ * - Получение изображения
+ * - Сохранение на диск
+ *
+ * @author Taneong
+ *
+ */
+
+public class Main {
+
+	/**
+	 * @param args Аргументы при запуске
+	 */
+	public static void main(String[] args) {
+		// Приветствие
+		System.out.println("EasyShot");
+		// Чтение настроек
+		Settings settings = new Settings();
+	}
+
+}
---
+++ Server/src/Reader.java
@@ -0,0 +1,8 @@
+/**
+ * Класс для прослушки порта
+ * @author Taneong
+ *
+ */
+public class Reader {
+
+}
